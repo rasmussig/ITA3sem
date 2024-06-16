cb7c6634202f39fdd4b5da5d2b7e38663a56b194
rasmussig <rasmus2001@live.dk>
2024-06-16 22:19:05 +0200

    NoLostDataWithEntityFrameWork

---
+++ RerunModuler/Modul6_EF_NoLostData/Migrations/20240616201220_NameAndCategoryToTitle.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Modul6_EF_NoLostData.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameAndCategoryToTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.AddColumn<string>(
+                            name: "Title",
+                            table: "Todo",
+                            nullable: true);
+
+            migrationBuilder.Sql(
+                @"UPDATE Todo
+                    SET Title = Category || ':' || Name;;
+                    ");
+
+            // TODO: Drop de to gamle kolonner
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Todo");
+
+            migrationBuilder.DropColumn(
+            name: "Category",
+            table: "Todo");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Todo",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Todo",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
